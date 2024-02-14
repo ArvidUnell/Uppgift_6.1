8c8d648d4a787b3514c846404e6dcc0248d2934a
ArvidUnell <arun06001@utb.vaxjo.se>
2024-02-14 12:29:53 +0100

    Add project files.

---
+++ Uppgift_6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static int Addera(int tal1, int tal2,int tal3)
+        {
+            return tal1 + tal2+tal3;
+        }
+    }
+}
